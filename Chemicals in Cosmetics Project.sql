4926815277f9dcf1ed5f918d9fb85e56de70b94a
Somtochukwu Omenkeukwu <116035995+Jaynifah@users.noreply.github.com>
2023-01-16 14:59:49 +0100

    Add files via upload

---
+++ Chemicals in Cosmetics Project.sql
@@ -0,0 +1,42 @@
+
+--1. Chemicals used the most in Cosmetics and Personl Care Products (Top 5)
+SELECT DISTINCT ChemicalName, COUNT(ChemicalName) Occurence
+FROM [Chemicals_in_cosmetics-]
+WHERE PrimaryCategory = 'Personal Care Products'
+GROUP BY ChemicalName
+ORDER BY Occurence DESC
+OFFSET 0 rows
+FETCH next 5 rows only
+
+--2. Find out which companies used the most reported chemicals in their cosmetics and personal care products.
+--CTE was used to fetch top 5 reported chemicals, then a select statement to find which companies used those chemicals
+WITH CTE_CHEMS AS (
+SELECT DISTINCT ChemicalName, COUNT(ChemicalName) Occurence
+FROM [Chemicals_in_cosmetics-]
+WHERE PrimaryCategory = 'Personal Care Products'
+GROUP BY ChemicalName
+ORDER BY Occurence DESC
+OFFSET 0 rows
+FETCH next 5 rows only
+)
+SELECT DISTINCT CiC.CompanyName, CTE.ChemicalName
+FROM [Chemicals_in_cosmetics-] CiC
+JOIN CTE_CHEMS CTE
+ON CiC.ChemicalName = CTE.ChemicalName
+
+
+
+--3. Brands with chemicals that were removed and discontinued
+SELECT BrandName, ChemicalName
+FROM [Chemicals_in_cosmetics-]
+WHERE DiscontinuedDate IS NOT NULL AND ChemicalDateRemoved IS NOT NULL
+ORDER BY BrandName
+
+--4. Brands with Chemicals that were mostly reported in 2018
+SELECT DISTINCT BrandName, ChemicalName, COUNT(BrandName) TimesReported
+FROM [Chemicals_in_cosmetics-]
+WHERE ChemicalCreatedAt LIKE '%2018%'
+GROUP BY BrandName,ChemicalName  
+ORDER BY TimesReported DESC
+
+
